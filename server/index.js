9031ac5dd8c1422fb8bfaaf012369fdc31509dbc
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2026-01-13 22:40:05 +0530

    Define the authentication routes

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
-dotenv.config();
+// Load environment variables from .env file
+// Change the path if your .env file is located elsewhere
+dotenv.config({ path: "../.env" });
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,6 +18,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Smart Agri Prediction System Server is running");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   connectDB();
