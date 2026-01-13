cb4a0f7dbc3ac8d014f4488fd1f05d6ade64d5f8
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2026-01-13 21:49:52 +0530

    MongoDB Connection Setup

---
+++ server/db/connectDB.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+// Function to connect to MongoDB
+export const connectDB = async () => {
+  // MongoDB connection URI
+  const MONGO_URI =
+    process.env.MONGO_URI ||
+    "mongodb://localhost:27017/smart-agri-prediction-system";
+
+  // Attempt to connect to MongoDB
+  try {
+    const conn = await mongoose.connect(MONGO_URI);
+    console.log(`MongoDB Connected Successfully: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -15,5 +17,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
