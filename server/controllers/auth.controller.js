309e11cf14a245fb291203a14f139c4e15dc1c25
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2026-01-14 14:24:21 +0530

    User Registers Controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,39 @@
+import bcryptjs from "bcryptjs";
+import { User } from "../models/user.model.js";
+
 export const register = async (req, res) => {
-  res.send("Register routes");
+  const { name, email, password } = req.body;
+
+  try {
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required!" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ success: false, message: "User already exists!" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const newUser = new User({ name, email, password: hashedPassword });
+
+    await newUser.save();
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Internal server error: ${error.message}`,
+    });
+  }
 };
 
 export const login = async (req, res) => {
