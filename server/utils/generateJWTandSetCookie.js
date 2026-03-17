0e0d95d43a92c57757b8c025c956f9fba283eb29
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2026-03-17 10:50:27 +0530

    Fixed: No token provided!, Issue

--- server/utils/generateJWTandSetCookie.js
+++ server/utils/generateJWTandSetCookie.js
@@ -1,3 +1,9 @@
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+// Change the path if your .env file is located elsewhere
+dotenv.config({ path: "../.env" });
+
 import jwt from "jsonwebtoken";
 
 export const generateJWTandSetCookie = (res, userId) => {
@@ -10,7 +16,7 @@ export const generateJWTandSetCookie = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true, // Accessible only by web server
     secure: process.env.NODE_ENV === "production", // Secure in production
-    sameSite: "strict", // CSRF protection
+    sameSite: "None", // CSRF protection for cross-site requests
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
